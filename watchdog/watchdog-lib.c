680967b9e303f99167f7cfa107682fa281b39372
Jay Dolan <jay.dolan@accesio.com>
2019-04-28 22:17:55 -0700

    Initial code sent out

--- watchdog/watchdog-lib.c
+++ watchdog/watchdog-lib.c
@@ -23,6 +23,7 @@ struct watchdog_status
                 uint8_t DI_Raw;
                 uint8_t DI_Debounced;
                 uint8_t DI_Rising; 
+
                 uint8_t DI_Falling;
                 uint16_t Temp_Counts;
                 uint16_t Humidity_Counts; 
@@ -41,10 +42,11 @@ struct watchdog_status
 };
 #pragma pack(pop)
 
-void aio_watchdog_read_status(int card_index, struct watchdog_status *stat);
+//Internal function declarations
+int aio_watchdog_read_status(int card_index, struct watchdog_status *stat);
 
 
-void aio_watchdog_library_init()
+int aio_watchdog_library_init()
 {
         int status = 0;
         if (!watchdog_library_init)
@@ -77,16 +79,24 @@ void aio_watchdog_library_init()
                         if ((usb_descriptor.idVendor == 0x1605) && (usb_descriptor.idProduct == 0x8080))
                         {
                                 status = libusb_open(device_list[i], &watchdog_card_list[opened]);
-                                printf("libusb_open status = %d\n", status);
+                                if (status < 0)
+                                {
+                                        printf("%s: libusb_open error \"%s\"\n", __FUNCTION__, libusb_error_name(status));
+                                }
                                 status = libusb_claim_interface(watchdog_card_list[opened], 0);
                                 opened++;
-                                printf("libusb_claim_interface status = %d\n", status);
+                                if (status < 0)
+                                {
+                                        printf("%s: libusb_claim_interface error \"%s\"\n", __FUNCTION__, libusb_error_name(status));
+                                }
                         }
                 }
 
                 watchdog_library_init = 1;
         }
+        return num_cards;
 }
+
 void aio_watchdog_library_term()
 {
         if(watchdog_library_init)
@@ -97,76 +107,178 @@ void aio_watchdog_library_term()
                 for ( i = 0 ; i < num_cards ; i++ )
                 {
                         status = libusb_release_interface(watchdog_card_list[i], 0);
-                        printf("libusb_release_interface = %d\n", status);
+                        if (status < 0)
+                        {
+                                printf("%s: libusb_release_interface error \"%s\"\n", __FUNCTION__, libusb_error_name(status));
+                        }
                         libusb_close(watchdog_card_list[i]);
                 }
 
                 libusb_exit(usb_context);
                 num_cards = 0;
-                
         }
 }
-void aio_watchdog_reset(int card_index)
+
+int aio_watchdog_reset(int card_index)
 {
         int status = 0;
-        status = libusb_control_transfer(watchdog_card_list[card_index], LIBUSB_REQUEST_TYPE_VENDOR, 0x40, 0, 0x52, NULL, 0, 0);
-        printf("%s: libusb_control_transfer status = %d\n", __FUNCTION__, status);
+        if (num_cards > card_index)
+        {
+                status = libusb_control_transfer(watchdog_card_list[card_index], LIBUSB_REQUEST_TYPE_VENDOR, 0x40, 0, 0x52, NULL, 0, 0);
+                if (status < 0)
+                {
+                        printf("%s: libusb_control_transfer error \"%s\"\n", __FUNCTION__, libusb_error_name(status));
+                }
+        }
+        else
+        {
+                printf("%s: Invalid card_index\n", __FUNCTION__);
+                status = -1;
+        }
+        return status;
 }
 
-void aio_watchdog_period_set (int card_index, int s)
+int aio_watchdog_period_set (int card_index, int s)
 {
-        int status = 0;
-        status = libusb_control_transfer(watchdog_card_list[card_index], LIBUSB_REQUEST_TYPE_VENDOR, 0x40, s, 0x54, NULL, 0, 0);
-        printf("%s: libusb_control_transfer status = %d\n", __FUNCTION__, status);
+                int status = 0;
+        if (num_cards > card_index)
+        {
+                status = libusb_control_transfer(watchdog_card_list[card_index], LIBUSB_REQUEST_TYPE_VENDOR, 0x40, s, 0x54, NULL, 0, 0);
+                if (status < 0)
+                {
+                        printf("%s: libusb_control_transfer error \"%s\"\n", __FUNCTION__, libusb_error_name(status));
+                }
+        }
+        else
+        {
+                printf("%s: Invalid card_index\n", __FUNCTION__);
+                status = -1;
+        }
+        return status;
 }
 
-void aio_watchdog_start (int card_index)
+int aio_watchdog_start (int card_index)
 {
         int status = 0;
-        status = libusb_control_transfer(watchdog_card_list[card_index], LIBUSB_REQUEST_TYPE_VENDOR, 0x40, 1, 0x44, NULL, 0, 0);
-        printf("%s: libusb_control_transfer status = %d\n", __FUNCTION__, status);
+        if (num_cards > card_index)
+        {
+                status = libusb_control_transfer(watchdog_card_list[card_index], LIBUSB_REQUEST_TYPE_VENDOR, 0x40, 1, 0x44, NULL, 0, 0);
+                if (status < 0)
+                {
+                        printf("%s: libusb_control_transfer error \"%s\"\n", __FUNCTION__, libusb_error_name(status));
+                }
+        }
+        else
+        {
+                printf("%s: Invalid card_index\n", __FUNCTION__);
+                status = -1;
+        }
+        return status;
 }
 
-void aio_watchdog_pet (int card_index)
+int aio_watchdog_pet (int card_index)
 {
-        int status = 0;
-        status = libusb_control_transfer(watchdog_card_list[card_index], LIBUSB_REQUEST_TYPE_VENDOR, 0x40, 1, 0x50, NULL, 0, 0);
-        printf("%s: libusb_control_transfer status = %d\n", __FUNCTION__, status);
+                int status = 0;
+        if (num_cards > card_index)
+        {
+                status = libusb_control_transfer(watchdog_card_list[card_index], LIBUSB_REQUEST_TYPE_VENDOR, 0x40, 1, 0x50, NULL, 0, 0);
+                if (status < 0)
+                {
+                        printf("%s: libusb_control_transfer error \"%s\"\n", __FUNCTION__, libusb_error_name(status));
+                }
+        }
+        else
+        {
+                printf("%s: Invalid card_index\n", __FUNCTION__);
+                status = -1;
+        }
+        return status;
 }
-void aio_watchdog_stop (int card_index)
+int aio_watchdog_stop (int card_index)
 {
         int status = 0;
-        status = libusb_control_transfer(watchdog_card_list[card_index], LIBUSB_REQUEST_TYPE_VENDOR, 0x40, 0, 0x44, NULL, 0, 0);
-        printf("%s: libusb_control_transfer status = %d\n", __FUNCTION__, status);
+        if (num_cards > card_index)
+        {
+                status = libusb_control_transfer(watchdog_card_list[card_index], LIBUSB_REQUEST_TYPE_VENDOR, 0x40, 0, 0x44, NULL, 0, 0);
+                if (status < 0)
+                {
+                        printf("%s: libusb_control_transfer error \"%s\"\n", __FUNCTION__, libusb_error_name(status));
+                }
+        }
+        else
+        {
+                printf("%s: Invalid card_index\n", __FUNCTION__);
+                status = -1;
+        }
+        return status;
 }
-void aio_watchdog_reset_time_set (int card_index, uint8_t pulse_width)
+int aio_watchdog_pulse_width_set (int card_index, uint8_t pulse_width)
 {
         int status = 0;
-        status = libusb_control_transfer(watchdog_card_list[card_index], LIBUSB_REQUEST_TYPE_VENDOR, 0x40, pulse_width, 0x45, NULL, 0, 0);
-        printf("%s: libusb_control_transfer status = %d\n", __FUNCTION__, status);
+        if (num_cards > card_index)
+        {
+                status = libusb_control_transfer(watchdog_card_list[card_index], LIBUSB_REQUEST_TYPE_VENDOR, 0x40, pulse_width, 0x45, NULL, 0, 0);
+                if (status < 0)
+                {
+                        printf("%s: libusb_control_transfer error \"%s\"\n", __FUNCTION__, libusb_error_name(status));
+                }
+        }
+        else
+        {
+                printf("%s: Invalid card_index\n", __FUNCTION__);
+                status = -1;
+        }
+        return status;
 }        
-void aio_watchdog_temp_read (int card_index, double *degrees_c)
-{
-        struct watchdog_status stat = { 0 };
-        aio_watchdog_read_status(card_index, &stat);
-        *degrees_c = 175.72/65536 * stat.r.Temp_Counts - 46.85;
-}
-void aio_watchdog_humidity_read (int card_index, double *humidity)
-{
-        struct watchdog_status stat = { 0 };
-        aio_watchdog_read_status(card_index, &stat);
-        *humidity = 125/65536 * stat.r.Humidity_Counts - 6;
-}
-void aio_watchdog_read_status(int card_index, struct watchdog_status *stat)
+int aio_watchdog_temp_read (int card_index, double *degrees_c)
 {
         int status = 0;
-        status = libusb_control_transfer(watchdog_card_list[card_index], 
-                                        LIBUSB_REQUEST_TYPE_VENDOR | 0x80, 
-                                        0x10, 
-                                        0, 
-                                        0x1, 
-                                        (unsigned char *)stat, 
-                                        sizeof(struct watchdog_status), 
-                                        0);
-        printf("%s: libusb_control_transfer status = %d, sizeof(struct watchdog_status = %lu\n", __FUNCTION__, status, sizeof(struct watchdog_status));
+        struct watchdog_status stat = { 0 };
+        status = aio_watchdog_read_status(card_index, &stat);
+        if (0 == status)
+        {
+                *degrees_c = 175.72/65536 * stat.r.Temp_Counts - 46.85;
+        }
+        return status;
+}
+int aio_watchdog_humidity_read (int card_index, double *humidity)
+{
+        int status = 0;
+        struct watchdog_status stat = { 0 };
+        aio_watchdog_read_status(card_index, &stat);
+        if (0 == status)
+        {
+                *humidity = 125/65536 * stat.r.Humidity_Counts - 6;
+        }
+        return status;
+}
+int aio_watchdog_read_status(int card_index, struct watchdog_status *stat)
+{
+        int status = 0;
+        if (num_cards > card_index)
+        {
+                status = libusb_control_transfer(watchdog_card_list[card_index], 
+                                LIBUSB_REQUEST_TYPE_VENDOR | 0x80, 
+                                0x10, 
+                                0, 
+                                0x1, 
+                                (unsigned char *)stat, 
+                                sizeof(struct watchdog_status), 
+                                0);
+                if (status < 0)
+                {
+                        printf("%s: libusb_control_transfer error \"%s\"\n", __FUNCTION__, libusb_error_name(status));
+                }
+                else
+                {
+                        status = 0;
+                }
+                
+        }
+        else
+        {
+                printf("%s: Invalid card_index\n", __FUNCTION__);
+                status = -1;
+        }
+        return status;
 }
--- watchdog/watchdog-lib.h
+++ watchdog/watchdog-lib.h
@@ -1,15 +1,26 @@
+#include <stdint.h>
 
 
 
 
-
-void aio_watchdog_library_init();
+/******************************
+ * This needs to be the first function called in the library. It returns the
+ * number of watchdog cards found.
+ * ***************************/
+int aio_watchdog_library_init();
 void aio_watchdog_library_term();
-void aio_watchdog_reset(int card_index);
-void aio_watchdog_period_set (int card_index, int s);
-void aio_watchdog_start (int card_index);
-void aio_watchdog_pet (int card_index);
-void aio_watchdog_stop (int card_index);
-void aio_watchdog_reset_time_set (int card_index, uint8_t pulse_width);
-void aio_watchdog_temp_read (int card_index, double *degrees_c);
-void aio_watchdog_humidity_read (int card_index, double *humidity);
+/*******************************
+ * All of these functions return 0 on success. If an error is encountered
+ * In the libusb function they wrap, they will print the error message to stdout
+ * and return the libusb error code.
+ * card_index starts at 0. If an invlid card_index is passed in it will print
+ * an error message to stdout and return -1
+ * ****************************/
+int aio_watchdog_reset(int card_index);
+int aio_watchdog_period_set (int card_index, int s);
+int aio_watchdog_start (int card_index);
+int aio_watchdog_pet (int card_index);
+int aio_watchdog_stop (int card_index);
+int aio_watchdog_pulse_width_set (int card_index, uint8_t pulse_width);
+int aio_watchdog_temp_read (int card_index, double *degrees_c);
+int aio_watchdog_humidity_read (int card_index, double *humidity);
