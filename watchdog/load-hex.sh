ca56be91de216ceacf2fe075eb25359f4f379984
Jay Dolan <jay.dolan@accesio.com>
2019-06-27 21:14:00 -0700

    script to load the firmware and chmod the new dev file

---
+++ watchdog/load-hex.sh
@@ -0,0 +1,11 @@
+DEVICE=`lsusb | grep 1605 | awk '{print $4}'`
+DEVICE="${DEVICE%%:}"
+
+sudo fxload -v -D /dev/bus/usb/003/$DEVICE -I $1  -t fx2lp
+sleep 3
+
+DEVICE=`lsusb | grep 1605 | awk '{print $4}'`
+DEVICE="${DEVICE%%:}"
+
+echo sudo chmod 777 /dev/bus/usb/003/$DEVICE
+sudo chmod 666 /dev/bus/usb/003/$DEVICE
